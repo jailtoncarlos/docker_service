8a6ddf607418e97a826658f777293d54ae4837d5
Jailton Paiva <jailtoncarlos@gmail.com>
2024-10-29 11:43:48 -0300

    ajustes finos nas imagens python

--- pre-commit-bin/print_statements.py
+++ pre-commit-bin/print_statements.py
@@ -2,61 +2,55 @@ from __future__ import annotations
 
 import argparse
 import ast
-import traceback
 import tokenize
-from typing import Sequence
+import traceback
+from typing import List, Optional, Sequence
 
-FILENAMES = (
-    'views.py',
-    'forms.py',
-    'tasks.py',
-    'models.py'
-)
+FILENAMES = ("views.py", "forms.py", "tasks.py", "models.py")
 
 
 def check_file(filename: str) -> int:
     try:
-        with open(filename, 'rb') as f:
+        with open(filename, "rb") as f:
             ast.parse(f.read(), filename=filename)
     except SyntaxError:
-        print(f'{filename} - Could not parse ast')
+        print(f"{filename} - Could not parse ast")
         print()
-        print('\t' + traceback.format_exc().replace('\n', '\n\t'))
+        print("	" + traceback.format_exc().replace("", ""))
         print()
         return 1
 
-    lines = []
+    lines: List[Optional[tokenize.TokenInfo]] = []
     with tokenize.open(filename) as f:
         tokens = tokenize.generate_tokens(f.readline)
         line = None
         for token in tokens:
-            # next token after finding statement
-            # should come before the line should be changed
             if line:
-                # if comment found at the same line, ignore statement
-                if token.start[0] == line.start[0] and token.type == 61 and token.string == '# NOQA':
+                if (
+                    token.start[0] == line.start[0]
+                    and token.type == 61
+                    and token.string == "# NOQA"
+                ):
                     line = None
                     continue
-
-                # next line from import with no previous comment
                 if token.start[0] > line.start[0]:
                     lines.append(line)
                     line = None
                     continue
-            # statement found
-            if token.string == 'print' and token.type == 1:
+            if token.string == "print" and token.type == 1:
                 line = token
                 continue
 
     for line in lines:
-        print(f'{filename}:{line.start[0]}:{line.start[1]}: {line.string} found')
+        if line is not None:
+            print(f"{filename}:{line.start[0]}:{line.start[1]}: {line.string} found")
 
     return int(bool(lines))
 
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*', help='Filenames to run')
+    parser.add_argument("filenames", nargs="*", help="Filenames to run")
     args = parser.parse_args(argv)
 
     retv = 0
@@ -67,5 +61,5 @@ def main(argv: Sequence[str] | None = None) -> int:
     return retv
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
--- pre-commit-bin/pylint.py
+++ pre-commit-bin/pylint.py
@@ -1,45 +1,68 @@
 #!/usr/bin/python3
 import os
 import shlex
-import sys
 import subprocess
+import sys
 
 
 def get_changed_files():
-    process = subprocess.Popen(shlex.split('git diff --name-only origin/master..'), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False)
-    files = []
-    for line in iter(process.stdout.readline, b''):
-        output = line.rstrip().decode()
-        if output.endswith('.py'):
-            files.append(output)
+    try:
+        process = subprocess.Popen(  # pylint: disable=R1732:
+            shlex.split("git diff --name-only"),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=False,
+        )
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+            raise Exception(stderr.decode())  # pylint: disable=W0719
+    except RuntimeError as exc:
+        sys.stderr.write(f"Erro durante a execução do pylint: {exc}")
+        sys.exit(1)
+
+    files = [
+        line.rstrip().decode() for line in stdout.splitlines() if line.endswith(b".py")
+    ]
     return files
 
 
 def main():
-    if '--all' in sys.argv:
-        apps = '.'
-        if '-v' in sys.argv or '--verbose' in sys.argv:
-            sys.stdout.write('>>> Todos os arquivos serão avaliados\n')
+    if "--all" in sys.argv:
+        apps = "."
+        if "-v" in sys.argv or "--verbose" in sys.argv:
+            sys.stdout.write(">>> All files will be evaluated\n")
     else:
         files = get_changed_files()
-        apps = ' '.join(file for file in files if os.path.exists(file))
-        if '-v' in sys.argv or '--verbose' in sys.argv:
-            sys.stdout.write('>>> Os seguintes arquivos serão avaliados:\n')
-            sys.stdout.write('\n'.join(files))
-            sys.stdout.write('\n')
+        apps = " ".join(file for file in files if os.path.exists(file))
+        if "-v" in sys.argv or "--verbose" in sys.argv:
+            sys.stdout.write(">>> The following files will be evaluated:\n")
+            sys.stdout.write("\n".join(files))
+            sys.stdout.write("\n")
     if not apps:
-        apps = '.'
+        apps = "."
 
-    result = subprocess.run(f'flake8 {apps} --ignore E501,W503,W291,E713,E712,E902 --exclude src/'.split(' '), capture_output=True)
-    stdout = result.stdout.decode('utf-8')
-    ok = len(stdout)
-    if ok > 0:
+    sys.stdout.write(f">>> Running pylint on {apps}\n")
+    result = subprocess.run(
+        shlex.split(f"pylint {apps} --rcfile=.pylintrc"),
+        capture_output=True,
+        check=False,  # Evita interrupções no processo.
+    )
+    if result.returncode != 0:
+        print(f"Pylint retornou código {result.returncode}")
+        print("Error:")
+        print(result.stderr)
+
+    stdout = result.stdout.decode("utf-8")
+    stderr = result.stderr.decode("utf-8")
+
+    if result.returncode != 0:
         sys.stderr.write(stdout)
-        sys.stderr.write('\nCorrija os erros do SUAP! :)\n')
+        sys.stderr.write(stderr)
+        sys.stderr.write("\nPlease fix the listed errors! :)\n")
         sys.exit(1)
     else:
-        print('>>>>>> Nenhuma advertência encontrada...')
+        print(">>>>>> No warnings found...")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
